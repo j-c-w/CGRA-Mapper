50e21ea909eaf65256720589109c606b928d3040
Jackson Woodruff <woodruff.jackson@gmail.com>
2022-05-14 15:41:30 -0700

    Script fixes

--- test/compile.sh
+++ test/compile.sh
@@ -1,2 +1,3 @@
+set -e
 clang -emit-llvm -fno-unroll-loops -O3 -o kernel.bc -c kernel.cpp
 #llvm-dis fir.bc -o fir.ll
--- test/run_tests_against.sh
+++ test/run_tests_against.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/bin/zsh
 
 set -eu
 
+typeset use_egraphs
+zparseopts -D -E -use-egraphs=use_egraphs
+
 if [[ $# -ne 2 ]]; then
 	echo "Usage: $0 <CGRA Design File> <Other Input Files Base Folder>"
+	echo "--use-egraphs to use egraph rewriting"
 	echo "Other files should all be in the format loopX.c (X a number)"
 	exit 1
 fi
 
 ## Build the CGRA.
 cp $1 kernel.cpp
+echo "Building kernel "
+cat kernel.cpp
 
 ## Build the initial CGRA.
 ./compile.sh
@@ -17,10 +23,24 @@ cp $1 kernel.cpp
 ./build_param.sh operations.json
 
 shift
+touch run_output
 
 ## Now run over all other input files.
 for file in $(find $1 -name "loop*.c"); do
+	echo "Building $file"
+	echo "Building $file " >> run_output
 	cp $file kernel.cpp
 	./compile.sh
-	(timeout 2 ./run.sh && echo "Succeeded in building $file") || echo "Failed to build $file for reason $?"
+	if [[ ${#use_egraphs} == 0 ]]; then
+		./run.sh &>> run_output
+	else
+		./run.sh --use-egraphs &>> run_output
+	fi
 done
+
+success=$(grep -ce "Mapping:success" run_output || echo "")
+fails=$(grep -ce "Mapping:fail" run_output || echo "")
+
+mv run_output run_output.old
+
+echo "Have $success successes, $fails fails"
