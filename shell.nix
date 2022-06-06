5d0da2cdf157962e110bca7ac2bba3a8b2f2f49f
Jackson Woodruff <woodruff.jackson@gmail.com>
2022-06-05 21:23:43 -0700

    Add some python for scripting

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@
 with pkgs;
 
 mkShell {
-	buildInputs = [ llvm clang cmake ctags gdb cargo rustc CoinMP ];
+	buildInputs = [ llvm clang cmake ctags gdb cargo rustc CoinMP 
+	  python39 python39Packages.apsw ];
 	SHELL_NAME="CGRA-Mapper";
 }
