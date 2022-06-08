e35da5cfb762bd7a3c900a30c70891e5c896763a
Jackson Woodruff <jacksonwoodruff@fb.com>
2022-06-08 08:37:33 -0700

    Try and make shell work with --pur

--- shell.nix
+++ shell.nix
@@ -3,7 +3,11 @@
 with pkgs;
 
 mkShell {
-	buildInputs = [ llvm clang cmake ctags gdb cargo rustc CoinMP 
-	  python39 python39Packages.apsw vim parallel ];
+	buildInputs = [
+		llvm clang cmake ctags gdb cargo rustc CoinMP 
+	# Script deps
+	zsh python39 python39Packages.apsw python39Packages.numpy python39Packages.matplotlib parallel 
+	# TOols to make developement within this shell bearable
+	git vim ];
 	SHELL_NAME="CGRA-Mapper";
 }
