64763630e7ba7dead437c29e118f504c33aec5c4
Jackson Woodruff <jacksonwoodruff@fb.com>
2022-06-24 16:38:50 -0700

    Fix compile issues with the shell

--- shell.nix
+++ shell.nix
@@ -5,7 +5,9 @@ with pkgs;
 mkShell {
 	buildInputs = [
 		llvm clang
-		cmake ctags gdb cargo rustc CoinMP 
+		cmake ctags gdb cargo rustc CoinMP
+		# Deps for the DFG Mapping
+		tclap
 	# Script deps
 	zsh python39 python39Packages.apsw python39Packages.numpy python39Packages.matplotlib parallel 
 	# TOols to make developement within this shell bearable
