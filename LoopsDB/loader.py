ac0871e474440860bd1e3727b5c9ba982838b549
Jackson Woodruff <jacksonwoodruff@fb.com>
2022-06-24 16:39:08 -0700

    Fix the loop loader

--- LoopsDB/loader.py
+++ LoopsDB/loader.py
@@ -12,16 +12,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    con = apsw.Connection('loops.db')
+    con = apsw.Connection(args.db)
     cur = con.cursor()
 
     counter = 0
     if os.path.exists(args.output_folder):
         shutil.rmtree(args.output_folder)
     os.mkdir(args.output_folder)
-    for s in cur.execute('select body from loops'):
+    for s in cur.execute('select distinct loops.src, loops.filename from loops where loops.meta_clang_returncode is 0 and loops.meta_stmt_counts_IfStmt is null and loops.meta_stmt_counts_CallExpr is null'):
         with open(args.output_folder + "/loop" + str(counter) + ".c", 'w') as f:
             program = s[0]
-            file = "TODO --- get source filename" # s[1]
+            file = s[1]
             f.write('// Source is: ' + file + '\n\n' + program)
         counter += 1
