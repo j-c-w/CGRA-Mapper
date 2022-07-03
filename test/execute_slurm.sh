7e4edbb7f4b57984d4bda422c029bc0bfaab8560
Jackson Woodruff <jacksonwoodruff@fb.com>
2022-07-02 22:04:16 -0700

    use batch as requested

--- test/execute_slurm.sh
+++ test/execute_slurm.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh
+#SBATCH --cpus-per-task 40
+#SBATCH --mem=400GB
+#SBATCH -t 360 
 
 typeset -a run_individual_rulesets
 zparseopts -D -E -- -run-individual-rulesets=run_individual_rulesets
 
-if [[ $# -ne 2 ]]; then
-	echo "Usage: $0 <loop number> <reduction rate>"
+if [[ $# -ne 4 ]]; then
+	echo "Usage: $0 <loop name> <loop number> <loops folder> <reduction rate>"
+	echo "Note: this should only be issued as a slurm job -- it doesn't issue the slurm jobs itself"
 	exit 1
 fi
 
@@ -14,8 +18,10 @@ if [[ ${#run_individual_rulesets} -gt 0 ]]; then
 fi
 
 mkdir -p temp_slurm
-loopno=$1
-reduction_rate=$2 # Float from 0.0 to 1.0 --- how many loops to try
+loopname=$1
+loopno=$2
+loops_folder=$3
+reduction_rate=$4 # Float from 0.0 to 1.0 --- how many loops to try
 # and compile to each design to save time.
 
 cd ~/nix
@@ -24,6 +30,6 @@ cd ~/Projects/CGRA/CGRA-Mapper
 nix-shell shell.nix --run '
 set -x
 cd test
-./run_all_benchmarks.sh $run_individual_rulesets_flag $reduction_rate ../Loops2 temp_slurm/temp$loopno --gather-only --test-loops loop$loopno.c
+./run_all_benchmarks.sh $run_individual_rulesets_flag $reduction_rate $loops_folder temp_slurm/temp$loopno --gather-only --test-loops $loopname
 '
 "
