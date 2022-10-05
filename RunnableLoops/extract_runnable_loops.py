86a5a66f59eba3100aad9ae1e20ef56fb1cff436
Jackson Woodruff <woodruff.jackson@gmail.com>
2022-10-05 05:49:53 -0700

    Try to fix more bugs

--- RunnableLoops/extract_runnable_loops.py
+++ RunnableLoops/extract_runnable_loops.py
@@ -34,6 +34,10 @@ if __name__ == "__main__":
     sources = []
     for s in cur.execute("""
         select loops.src, loops.src_with_call from loops
+        where meta_clang_returncode is 0
+        and loops.meta_stmt_counts_IfStmt is null and
+        loops.meta_stmt_counts_CallExtr is null and
+        loops.meta_project != 'libav'
     """):
         source = s[0]
         runnable = s[1]
@@ -43,9 +47,17 @@ if __name__ == "__main__":
     loops_string = ""
     runfuns = []
     imports = set()
+    used_fnames = set()
 
     for loop in sources:
         runnable = loop.runnable
+        # There are some dups in the set.
+        # Dont' need to measure those twice.
+        if loop.fname in used_fnames:
+            continue
+        else:
+            used_fnames.add(loop.fname)
+
         # We do some rewriting to the runnable one for tracking.
         runnable = runnable.replace('fn (', loop.fname + ' (')
         runnable = runnable.replace('fn(', loop.fname + '(')
@@ -66,10 +78,26 @@ if __name__ == "__main__":
 
         # add #define to track how many times the loop executed.
         insert_index = re.finditer('for (.*)[ \t\n]*?{', runnable)
+        match_found = False
         for match in insert_index:
+            match_found = True
             # Insert the 'define after end.
             end = match.end(0)
             runnable = runnable[0:end] + "\nLOOP_ITERATION;\n" + runnable[end:]
+
+        if not match_found:
+            # For single-line loops, it generates:
+            # for (...)
+            #   code
+            # We need to do more complex substitution.
+            insert_index = re.finditer('for ([^\n]*)\n', runnable)
+            tail_insert_index = re.finditer('for ([^\n])\n.*?\n', runnable)
+            for match in insert_index:
+                end = match.end(0)
+                runnable = runnable[:end] + "{\nLOOP_ITERATION;\n" + runnable[end:]
+            for match in tail_insert_index:
+                end = match.end(0)
+                runnable = runnable[:end] + ";}" + runnable[end:]
 
         # Write the loop individually into a matched source
         # file so we can get the CGRA-Mapper compile time.
